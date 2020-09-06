9b15fc7fe9cf4432bb678622bf738b81bd08525e
Ramin Hashemi <raminhashemi9@gmail.com>
2020-09-06 12:19:40 +0430

    Website template

---
+++ README.md
@@ -0,0 +1,6 @@
+# hashemi.github.io
+Hello, welcome to my website. [hashemi.github.io](https://hashemi.github.io)
+
+Created with the help of [Bootstrap](http://getbootstrap.com)
+
+If you want to give me your own suggestions email me on `raminhashemi9@gmail.com`
---
+++ structuredata.js
@@ -0,0 +1,24 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "familyName": "Jojare",
+  "givenName": "Saurabh",
+  "url": "https://saurabhjojare.github.io",
+  "email": "saurabh.jojare@gmail.com",
+  "affiliation": "Savitribai Phule Pune University",
+  "birthDate": "2 Feb 2000",
+  "gender": "http://schema.org/Male",
+  "hasOccupation": "Full Stack Developer",
+  "nationality": "Indian",
+  "image": "https://avatars1.githubusercontent.com/u/32615499?s=460&v=4",
+  
+ 
+  "sameAs": [
+    "https://twitter.com/saurabhjojare",
+    "https://www.instagram.com/saurabh_jojare/",
+    "https://www.linkedin.com/in/saurabhjojare/"
+    "https://github.com/saurabhjojare"
+    ]
+}
+</script>
